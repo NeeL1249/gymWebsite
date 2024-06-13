71e73b34651f591a4f8d1b776ec06d804f3d30d8
NeeL1249 <neelmulik12@gmail.com>
2024-06-14 01:19:40 +0530

    Added get queries route and function.

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,10 +1,11 @@
 const {Router} = require('express');
 const { checkIfAdmin } = require('../middlewares/user.middleware');
-const { createBlog, updateBlog, deleteBlog } = require('../controllers/admin.controller');
+const { getQueries, createBlog, updateBlog, deleteBlog } = require('../controllers/admin.controller');
 
 const router = Router();
 
 router.post('/postBlog', checkIfAdmin, createBlog);
+router.get('/queries', checkIfAdmin, getQueries);
 router.patch('/:blogId/updateBlog', checkIfAdmin, updateBlog);
 router.delete('/:blogId/deleteBlog', checkIfAdmin, deleteBlog);
 
